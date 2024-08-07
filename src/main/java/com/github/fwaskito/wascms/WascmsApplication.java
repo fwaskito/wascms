7c7af19677c7db6c459d998b851d3150eb5be434
F. Waskito <fajar.waskito10@gmail.com>
2024-08-07 23:30:02 +0700

    chore: add Spring Boot application runner

---
+++ src/main/java/com/github/fwaskito/wascms/WascmsApplication.java
@@ -0,0 +1,13 @@
+package com.github.fwaskito.wascms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WascmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WascmsApplication.class, args);
+	}
+
+}
